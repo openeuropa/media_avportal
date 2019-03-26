7640e6d75e62503ee8a250bd66e8de6c7ed1230c
upchuk <danny@webomelette.com>
2019-03-26 09:51:51 +0100

    OPENEUROPA-1751: Stream wrapper for the remote photos.

--- src/Plugin/Field/FieldFormatter/AvPortalPhotoFormatter.php
+++ src/Plugin/Field/FieldFormatter/AvPortalPhotoFormatter.php
@@ -4,11 +4,14 @@ declare(strict_types = 1);
 
 namespace Drupal\media_avportal\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
@@ -53,6 +56,13 @@ class AvPortalPhotoFormatter extends FormatterBase implements ContainerFactoryPl
   protected $logger;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * Constructs an AvPortalPhotoFormatter instance.
    *
    * @param string $plugin_id
@@ -77,15 +87,18 @@ class AvPortalPhotoFormatter extends FormatterBase implements ContainerFactoryPl
    *   The AV Portal client.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    *   The config factory.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
    *
    * @SuppressWarnings(PHPMD.ExcessiveParameterList)
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, MessengerInterface $messenger, LoggerChannelFactoryInterface $logger_factory, AvPortalClientInterface $avPortalClient, ConfigFactoryInterface $configFactory) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, MessengerInterface $messenger, LoggerChannelFactoryInterface $logger_factory, AvPortalClientInterface $avPortalClient, ConfigFactoryInterface $configFactory, EntityTypeManagerInterface $entityTypeManager) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
     $this->messenger = $messenger;
     $this->logger = $logger_factory->get('media');
     $this->avPortalClient = $avPortalClient;
     $this->config = $configFactory->get('media_avportal.settings');
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
@@ -103,8 +116,57 @@ class AvPortalPhotoFormatter extends FormatterBase implements ContainerFactoryPl
       $container->get('messenger'),
       $container->get('logger.factory'),
       $container->get('media_avportal.client'),
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'image_style' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $image_styles = image_style_options(FALSE);
+
+    $element['image_style'] = [
+      '#title' => $this->t('Image style'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('image_style'),
+      '#empty_option' => $this->t('None (original image)'),
+      '#options' => $image_styles,
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    $image_styles = image_style_options(FALSE);
+    // Unset possible 'No defined styles' option.
+    unset($image_styles['']);
+    // Styles could be lost because of enabled/disabled modules that defines
+    // their styles in code.
+    $image_style_setting = $this->getSetting('image_style');
+    if (isset($image_styles[$image_style_setting])) {
+      $summary[] = $this->t('Image style: @style', ['@style' => $image_styles[$image_style_setting]]);
+    }
+    else {
+      $summary[] = $this->t('Original image');
+    }
+
+    return $summary;
   }
 
   /**
@@ -133,11 +195,17 @@ class AvPortalPhotoFormatter extends FormatterBase implements ContainerFactoryPl
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode): array {
+    $cache = new CacheableMetadata();
+    $list_cache_tags = $this->entityTypeManager->getDefinition('image_style')->getListCacheTags();
+    $cache->addCacheTags($list_cache_tags);
+
     $elements = [];
 
     foreach ($items as $delta => $item) {
       $elements[$delta] = $this->viewElement($item);
     }
+
+    $cache->applyTo($elements);
 
     return $elements;
   }
@@ -170,11 +238,61 @@ class AvPortalPhotoFormatter extends FormatterBase implements ContainerFactoryPl
       return [];
     }
 
-    return [
-      '#theme' => 'image',
+    $cache = new CacheableMetadata();
+
+    $image_style = $this->settings['image_style'];
+    $theme = $image_style !== "" ? 'image_style' : 'image';
+
+    $build = [
+      '#theme' => $theme,
       '#attributes' => ['class' => ['avportal-photo']],
-      '#uri' => $this->config->get('photos_base_uri') . $resource->getPhotoUri(),
+      '#uri' => 'avportal://' . $resource_ref . '.jpg',
     ];
+
+    if ($theme === 'image_style') {
+      $build['#style_name'] = $image_style;
+      $cache->addCacheTags($this->entityTypeManager->getStorage('image_style')->load($image_style)->getCacheTags());
+    }
+
+    $cache->applyTo($build);
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies(): array {
+    $dependencies = parent::calculateDependencies();
+    $style_id = $this->getSetting('image_style');
+    /** @var \Drupal\image\ImageStyleInterface $style */
+    if ($style_id && $style = $this->entityTypeManager->getStorage('image_style')->load($style_id)) {
+      $dependencies[$style->getConfigDependencyKey()][] = $style->getConfigDependencyName();
+    }
+
+    return $dependencies;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onDependencyRemoval(array $dependencies): bool {
+    $changed = parent::onDependencyRemoval($dependencies);
+    $style_id = $this->getSetting('image_style');
+    /** @var \Drupal\image\ImageStyleStorageInterface $storage */
+    $storage = $this->entityTypeManager->getStorage('image_style');
+    /** @var \Drupal\image\ImageStyleInterface $style */
+    if ($style_id && $style = $storage->load($style_id)) {
+      if (!empty($dependencies[$style->getConfigDependencyKey()][$style->getConfigDependencyName()])) {
+        $replacement_id = $storage->getReplacementId($style_id);
+        if ($replacement_id && $storage->load($replacement_id)) {
+          $this->setSetting('image_style', $replacement_id);
+          $changed = TRUE;
+        }
+      }
+    }
+
+    return $changed;
   }
 
 }
---
+++ tests/src/Kernel/AvPortalStreamWrapperTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\media_avportal\src\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\media_avportal\AvPortalResource;
+
+/**
+ * Tests the AVPortalPhotoWrapper.
+ */
+class AvPortalStreamWrapperTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'system',
+    'media_avportal',
+    'media_avportal_mock',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installConfig(['media_avportal']);
+  }
+
+  /**
+   * Tests the basic methods of the AvPortalPhotoWrapper.
+   */
+  public function testBasicWrapperMethods(): void {
+    // Load a resource from the Mock and assert we have it.
+    $resource = $this->container->get('media_avportal.client')->getResource('P-038924/00-15');
+    $this->assertInstanceOf(AvPortalResource::class, $resource);
+
+    // Change the base URI of the photo store so that our stream wrapper points
+    // to our local site instead of the remote server.
+    $config = $this->container->get('config.factory')->getEditable('media_avportal.settings');
+    $config->set('photos_base_uri', getenv('SIMPLETEST_BASE_URL') . '/modules/custom/media_avportal/tests/fixtures/');
+    $config->save();
+
+    // file_get_contents() tests both the stream_open and getExternalUrl()
+    // methods of the stream wrapper.
+    $image = file_get_contents('avportal://P-038924/00-15.jpg');
+    $this->assertNotFalse($image);
+
+    // Test the url_stat() method of the stream wrapper.
+    $this->assertTrue(file_exists('avportal://P-038924/00-15.jpg'));
+  }
+
+}
