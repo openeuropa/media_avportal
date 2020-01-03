bcbca5678f9d36618ebc1bff954ccf8ca341e089
Francesco Sardara <francesco.sardara@ausy.be>
2020-01-03 14:28:43 +0100

    OPENEUROPA-2313: Improve the test coverage by verifying the response returned by cached requests.

--- tests/src/Kernel/AvPortalClientTest.php
+++ tests/src/Kernel/AvPortalClientTest.php
@@ -37,34 +37,42 @@ class AvPortalClientTest extends KernelTestBase {
     $history_middleware = $this->container->get('media_avportal_mock.history_middleware');
 
     $client = $this->container->get('media_avportal.client');
-    $client->query(['ref' => 'I-053547']);
+    $response = $client->query(['ref' => 'I-053547']);
     // One HTTP request has been fired to the AV Portal service.
     $this->assertCount(1, $history_middleware->getHistoryContainer());
 
     // Requesting the same resource should not launch a new HTTP request.
-    $client->query(['ref' => 'I-053547']);
+    $cached_response = $client->query(['ref' => 'I-053547']);
     $this->assertCount(1, $history_middleware->getHistoryContainer());
+    $this->assertEquals($response, $cached_response);
 
-    // Requesting another resource would trigger another request.
+    // Requesting another resource would trigger a new HTTP request.
+    $response = $client->query(['ref' => 'P-038924/00-15']);
+    $this->assertCount(2, $history_middleware->getHistoryContainer());
+    $this->assertNotEquals($cached_response, $response);
+
+    // The latest request has been cached too.
     $client->query(['ref' => 'P-038924/00-15']);
     $this->assertCount(2, $history_middleware->getHistoryContainer());
-    // The request has been cached too.
-    $client->query(['ref' => 'P-038924/00-15']);
+
+    // Cache entries use unique IDs. Request again the first resource to verify
+    // that the correct cached response is returned.
+    $response = $client->query(['ref' => 'I-053547']);
     $this->assertCount(2, $history_middleware->getHistoryContainer());
+    $this->assertEquals($cached_response, $response);
 
-    // Force a new request by disabling the cache for this query.
-    $client->query(['ref' => 'P-038924/00-15'], FALSE);
-    $this->assertCount(3, $history_middleware->getHistoryContainer());
-
-    // Verify that the cache IDs are different. Request again the first resource
-    // to verify that is still cached.
-    $client->query(['ref' => 'I-053547']);
-    $this->assertCount(3, $history_middleware->getHistoryContainer());
-
-    // All the request parameters are taken into account to generate the cache
-    // IDs.
+    // All request parameters are taken into account to generate the cache IDs.
     $client->query(['ref' => 'I-053547', 'random_parameter' => 1]);
+    $this->assertCount(3, $history_middleware->getHistoryContainer());
+
+    // Make a request with cache forcefully disabled.
+    $client->query(['ref' => 'P-039162|00-12'], FALSE);
     $this->assertCount(4, $history_middleware->getHistoryContainer());
+
+    // Make the same request again but let the caches to be used. A new HTTP
+    // request will be made, since the previous one was not stored.
+    $client->query(['ref' => 'P-039162|00-12']);
+    $this->assertCount(5, $history_middleware->getHistoryContainer());
   }
 
 }
